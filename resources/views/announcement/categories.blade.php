f47866449d3f518eca07035317e525b812ddb8e1
Fenslau <c01@bk.ru>
2025-05-06 12:46:50 +0700

    minor fix

--- resources/views/announcement/categories.blade.php
+++ resources/views/announcement/categories.blade.php
@@ -1,13 +1,24 @@
 <a class="mx-1 align-middle link-info"
     href="{{ route('announcements.index', ['care_category' => $announcement->careCategory->title]) }}">
-    <span data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="{{ $announcement->careCategory->description }}">
+    <span
+        @if($announcement->careCategory->description)
+        data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="{{ $announcement->careCategory->description }}"
+        @endif>
         {{ $announcement->careCategory->title }}
     </span>
 </a>
 
-@foreach ($announcement->multi_care_subcategory ?? [] as $multiCareSubcategory)
-<a href="{{ route('announcements.index', ['multi_care_subcategory' => \App\Models\CareCategory::where('id', $multiCareSubcategory)->first()?->title]) }}"
+@php
+$multiCareSubcategories = \App\Models\CareCategory::whereIn('id', $announcement->multi_care_subcategory ?? [])->get()->keyBy('id');
+@endphp
+@foreach ($multiCareSubcategories as $multiCareSubcategory)
+<a href="{{ route('announcements.index', ['multi_care_subcategory' => $multiCareSubcategory->title]) }}"
     class="btn btn-outline-info btn-sm py-0 small">
-    {{ \App\Models\CareCategory::where('id', $multiCareSubcategory)->first()?->title }}
+    <span
+        @if($multiCareSubcategory->description)
+        data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="{{ $multiCareSubcategory->description }}"
+        @endif>
+        {{ $multiCareSubcategory->title }}
+    </span>
 </a>
 @endforeach
